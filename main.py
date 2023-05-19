fb380147b3c5bc30f22642b5bb6f79f2b0a5a454
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-05-20 00:25:35 +0900

    add ics download

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import datetime
+import os
 import urllib.parse
 
 import uvicorn
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+
+from src.ics_util import export_ics
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -14,30 +17,45 @@ async def root():
     return RedirectResponse(url="https://github.com/minna-de-hack/schedule-api")
 
 @app.get("/api")
-async def makeschedule(request: Request, title: str = "", description: str = "", dates: str = ""):
-    context = {"request": request, "title": title, "description": description}
+async def make_schedule(request: Request, title: str = "", description: str = "", dates: str = ""):
+    context = {"request": request, "title": title, "description": description, "dates":dates}
 
     if dates != "":
         try:
-            start, stop = dates.split("/")
+            start, end = dates.split("/")
             start = datetime.datetime.strptime(start, '%Y%m%dT%H%M%S')
-            stop = datetime.datetime.strptime(stop, '%Y%m%dT%H%M%S')
+            end = datetime.datetime.strptime(end, '%Y%m%dT%H%M%S')
             context["start"] = start.strftime('%Y/%m/%d %H:%M')
-            context["stop"] = stop.strftime('%Y/%m/%d %H:%M')
+            context["end"] = end.strftime('%Y/%m/%d %H:%M')
         except:
             dates = ""
-            context["start"], context["stop"] = "", ""
+            context["start"], context["end"] = "", ""
 
     baseurl = "https://www.google.com/calendar/render?"
     d = {"action" : "TEMPLATE", "text" : title,
          "details" : description, "dates" : dates}
     d_qs = urllib.parse.urlencode(d)
     context["url"] = baseurl + d_qs
+    context["export_url"] = "/api/export?" + urllib.parse.urlencode({"title" : title, "description" : description, "dates" : dates})
 
     return templates.TemplateResponse("index.html", context)
 
+# http://localhost/api/export?title=sample+title&description=Today+is+Friday+in+California&dates=20201201T120000/20201201T121000
+@app.get("/api/export")
+async def export_schedule(title: str = "", description: str = "", dates: str = ""):
+    try:
+        fpath = export_ics(title, description, dates)
+    except:
+        pass
+
+    response = FileResponse(
+        path=fpath,
+        filename=os.path.split(fpath)[-1])
+
+    return response
+
 # http://localhost/sample/
-@app.get("/sample/", response_class=HTMLResponse)
+@app.get("/sample", response_class=HTMLResponse)
 async def sample():
     return """
     <html>
